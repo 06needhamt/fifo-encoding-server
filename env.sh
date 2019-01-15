c6049d90609e8fa06d7753b11b93034bc3cc1caf
Tom Needham <06needhamt@gmail.com>
2019-01-15 11:56:52 +0000

    Add Script To Automatically Build Dependencies

--- env.sh
+++ env.sh
@@ -1,6 +1,9 @@
 #! /bin/sh
+
+deps() { rm -rf deps && mkdir deps && cd deps && wget http://www.digip.org/jansson/releases/jansson-2.12.tar.gz && tar xvf jansson-2.12.tar.gz && cd jansson-2.12 && autoreconf -fi && ./configure &&  make && make check && sudo make install && cd ../../; }
 b() { make clean && make && bin/fifoserver; }
 v() { bin/fifoserver; }
 d() { gdb bin/fifoserver; }
 db() { make clean && make && gdb bin/fifoserver; }
 c() { make clean; }
+
