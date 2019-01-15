e49d6fb2438787a6ec623c3c79f172c0e3930121
Tom Needham <06needhamt@gmail.com>
2019-01-15 12:43:29 +0000

    Run Pre-Start Tests Before Starting Server

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <pthread.h>
 #include <unistd.h>
 
@@ -10,19 +11,79 @@
 #include <queue_item.h>
 #include <io.h>
 
-guid_t guid;
 pthread_t tid[2];
-queue_t q;
-queue_item_t item; 
-
-FILE* file;
+int err = 1;
 
 void* start_server(void* ptr);
-void* start_client(void* ptr);
+void* start_main_thread(void* ptr);
+int pre_start_tests(void);
+
+void* main_status;
+void* server_status;
 
 int main(int argc, char *argv[])
 {
-	int err = 0;
+	main_status = malloc(sizeof(int));
+	server_status = malloc(sizeof(int));
+
+	err = pthread_create(&(tid[0]), NULL, &start_main_thread, NULL);
+	if (err != 0) {
+		printf("\n can't create thread :[%s] \n", strerror(err));
+		exit(1);
+	}
+	else {
+		printf("Main Thread created successfully \n");
+	}
+
+	pthread_join(tid[0], NULL);
+	pthread_join(tid[1], NULL);
+
+	int status = (*(int*) server_status) & *((int*) main_status); 
+
+	free(server_status);
+	free(main_status);
+
+	if(!status)
+		return EXIT_FAILURE;
+
+	return EXIT_SUCCESS;
+}
+
+void* start_server(void* ptr) {
+	printf("Starting Server... \n");
+	start_websocket_server();
+
+	return NULL;
+}
+
+void* start_main_thread(void* ptr) {
+	printf("Starting Main Thread... \n");
+	printf("Running Pre Start Tests \n");
+
+	if(!pre_start_tests())
+		return false;
+
+	printf("Pre Start Tests Passed! \n");
+
+	err = pthread_create(&(tid[1]), NULL, &start_server, NULL);
+	if (err != 0) {
+		printf("\n can't create thread :[%s] \n", strerror(err));
+		exit(1);
+	}
+	else {
+		printf("Server Thread created successfully \n");
+	}
+
+	return true;
+}
+
+int pre_start_tests() {
+	guid_t guid;
+	queue_t q;
+	queue_item_t item; 
+
+	FILE* file;
+	
 	create_new_guid(&guid);
 	printf("Guid = %s \n", guid.value);
 
@@ -63,40 +124,7 @@ int main(int argc, char *argv[])
 	//push_item(&q, NULL);
 	
 	printf("Command = %s \n", item.command);
-
-	err = pthread_create(&(tid[0]), NULL, &start_server, NULL);
-	if (err != 0) {
-		printf("\n can't create thread :[%s] \n", strerror(err));
-		exit(1);
-	}
-	else {
-		printf("Server Thread created successfully \n");
-	}
-
-	err = pthread_create(&(tid[1]), NULL, &start_client, NULL);
-	if (err != 0) {
-		printf("\n can't create thread :[%s] \n", strerror(err));
-		exit(1);
-	}
-	else {
-		printf("Client Thread created successfully \n");
-	}
-
-	pthread_join(tid[0], NULL);
-	pthread_join(tid[1], NULL);
-
-	return EXIT_SUCCESS;
-}
-
-void* start_server(void* ptr) {
-	printf("Starting Server... \n");
-	start_websocket_server();
-
-	return NULL;
-}
-
-void* start_client(void* ptr) {
-	printf("Starting Client... \n");
-
-	return NULL;
+	
+	remove("queue.json");
+	return true;
 }
