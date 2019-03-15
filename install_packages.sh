a32281f8d239dbe4e6dbc68208df42ca81d67e6d
Tom Needham <06needhamt@gmail.com>
2019-03-15 18:34:06 +0000

    Update Dependent Packages

--- install_packages.sh
+++ install_packages.sh
@@ -1,3 +1,3 @@
 sudo apt-get update;
 sudo apt-get upgrade;
-sudo apt-get install cmake autoconf libavutil-dev libswresample-dev libavcodec-dev libavformat-dev libavfilter-dev libmicrohttpd-dev libssl-dev nvidia-cuda-toolkit;
+sudo apt-get install cmake autoconf libavutil-dev libswresample-dev libavcodec-dev libavformat-dev libavfilter-dev libmicrohttpd-dev libssl-dev valgrind nvidia-cuda-toolkit;
