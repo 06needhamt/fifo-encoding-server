d75304d79dd0f78b8953df4314e1798de21d487a
Tom Needham <06needhamt@gmail.com>
2019-03-20 10:42:17 +0000

    Switch to Stable FFMPEG Branch

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-deps() { ./install_packages.sh && rm -rf deps && mkdir deps && cd deps && ../build_gcc.sh && ../build_ffmpeg.sh && ../build_core_deps.sh && cd .. && mkdir bin/ && mkdir obj/; }
+deps() { ./install_packages.sh && sudo rm -rf deps && mkdir deps && cd deps && ../build_gcc.sh && ../build_ffmpeg.sh && ../build_core_deps.sh && cd .. && mkdir bin/ && mkdir obj/; }
 b() { make clean && sudo make && LD_LIBRARY_PATH=/usr/local/lib bin/fifoserver; }
 v() { make clean && sudo make && LD_LIBRARY_PATH=/usr/local/lib valgrind bin/fifoserver; }
 r() { LD_LIBRARY_PATH=usr/local/lib bin/fifoserver; }
